05d35a2efc39edd1e6479c98976721f58b77e8ad
webapp-pro <130032001+webapp-pro@users.noreply.github.com>
2023-10-12 02:56:32 -0700

    'repair'

---
+++ _next/static/gsyONmYLn5WFs1ccmfamR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/125-18a2d8c7cf87b592.js","static/chunks/pages/index-fa0ce8cc2b893b05.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
