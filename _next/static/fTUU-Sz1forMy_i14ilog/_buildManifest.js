7e19cfb5b6b804ca56e78f0124d5ae2333e869f0
webapp-pro <130032001+webapp-pro@users.noreply.github.com>
2023-10-16 02:27:35 -0700

    'lazy'

---
+++ _next/static/fTUU-Sz1forMy_i14ilog/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/924-0b024d763d1d8d3e.js","static/chunks/pages/index-3994572aba39a8a6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
